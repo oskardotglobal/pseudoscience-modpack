59f7bb367e53ccf1044ae3a58a344b2c229be4e1
Emory <107522398+emorydeer2@users.noreply.github.com>
2025-01-09 01:24:12 +0000

    fix: moth fuzz replaces thigh highs, improved mobs common.json entity flag config no longer has errors

--- kubejs/server_scripts/nothighhighs.js
+++ kubejs/server_scripts/nothighhighs.js
@@ -1,6 +1,6 @@
 
 LootJS.modifiers(event => {
     event.addLootTableModifier(/.*/)
-        .replaceLoot('estrogen:thigh_highs', 'estrogen:moth_fuzz')
+        .replaceLoot('estrogen:thigh_highs', 'estrogen:moth_fuzz', true)
 })
-// replace thigh highs with moss
+// moth fuzz makes sooo much more sense (not really for shipwrecks, but whatever.)
